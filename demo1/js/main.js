5a0f38c5ea7cfeb876329728744fd5b70857b179
husida <443827004@qq.com>
2018-03-23 18:11:24 +0800

    some change

--- demo1/js/main.js
+++ demo1/js/main.js
@@ -1,3 +1,4 @@
+/// <reference path="./phaser.d.ts"/>
 var width = window.innerWidth;
 var height = window.innerHeight;
 
@@ -8,39 +9,158 @@ var game = new Phaser.Game(width, height, Phaser.auto, '#game');
 var states = {
     // 加载
     preload: function () {
-        console.log('preload');
-        this.create = function () {
-            this.stage.backgroundColor = '#aaa';
+        this.preload = function () {
+            // 设置背景为黑色
+            game.stage.backgroundColor = '#000000';
+            // 加载游戏资源
+            game.load.crossOrigin = 'anonymous'; // 设置跨域
+            game.load.image('bg', '//24haowan-cdn.shanyougame.com/pickApple2/assets/images/bg.png');
+            game.load.image('dude', '//24haowan-cdn.shanyougame.com/pickApple2/assets/images/dude.png');
+            game.load.image('green', '//24haowan-cdn.shanyougame.com/pickApple2/assets/images/green.png');
+            game.load.image('red', '//24haowan-cdn.shanyougame.com/pickApple2/assets/images/red.png');
+            game.load.image('yellow', '//24haowan-cdn.shanyougame.com/pickApple2/assets/images/yellow.png');
+            game.load.image('bomb', '//24haowan-cdn.shanyougame.com/pickApple2/assets/images/bomb.png');
+            game.load.image('five', '//24haowan-cdn.shanyougame.com/pickApple2/assets/images/five.png');
+            game.load.image('three', '//24haowan-cdn.shanyougame.com/pickApple2/assets/images/three.png');
+            game.load.image('one', '//24haowan-cdn.shanyougame.com/pickApple2/assets/images/one.png');
+            game.load.audio('bgMusic', '//24haowan-cdn.shanyougame.com/pickApple2/assets/audio/bgMusic.mp3');
+           
+            // 添加加载进度文字
+            var processText = game.add.text(game.world.centerX, game.world.centerY, '0%', {
+                fontSize: '60px',
+                fill: '#ffffff'
+            });
+            
+            // 设置文字锚点到中心
+            processText.anchor.setTo(0.5,0.5);
+
+            // 监听加载完一个文件的事件
+            game.load.onFileComplete.add(function(process) {
+                processText.text = process + '%'
+
+            })
+
+            // 加载完成监听
+            game.load.onLoadComplete.add(load);
+
+            // 定义加载最小时间
+            var overMinTime = false;
             setTimeout(function() {
-                this.game.state.start('created');
-            }, 2000);
+                overMinTime = true;
+            }, 3000);
+
+            function load() {
+                if (overMinTime) {
+                    game.state.start('created');
+                } else {
+                    setTimeout(function () {
+                        load();
+                    }, 1000);
+                }
+            }
         }
     },
     // 开始
     created: function () {
-        console.log('created');
         this.create = function () {
-            this.stage.backgroundColor = '#444';
-            setTimeout(function() {
-                this.game.state.start('play');
-            }, 2000);
-            
+            // 添加背景
+            var bg = game.add.image(0, 0, 'bg');
+            bg.width = game.world.width;
+            bg.height = game.world.height;
+
+            var title = game.add.text(game.world.centerX, game.world.height * 0.25, '接苹果',{
+                fontSize: '40px',
+                fontWeight: 'bold',
+                fill: '#f2bb15'
+            });
+            title.anchor.setTo(0.5, 0.5);
+
+            var remind = game.add.text(game.world.centerX, game.world.centerY, '点击任意位置开始',{
+                fontSize: '20px',
+                fill: '#f2bb15'
+            });
+            remind.anchor.setTo(0.5, 0.5);
+
+            // 主角
+            var man = game.add.sprite(game.world.centerX, game.world.height * 0.75, 'dude');
+            var manImage = game.cache.getImage('dude');
+            man.width = game.world.width * 0.2;
+            man.height = man.width/manImage.width * manImage.height;
+            man.anchor.setTo(0.5, 0.5);
+
+            // 游戏点击事件
+            game.input.onTap.add(function () {
+                game.state.start('play');
+            });
         }
+
     },
     // 游戏中
     play: function () {
-        console.log('play');
         this.create = function () {
-            this.stage.backgroundColor = '#888';
-            setTimeout(function() {
-                this.game.state.start('over');
-            }, 2000);
+            var score = 0;
+            // 背景音乐
+            var bgMusic = game.add.audio('bgMusic');
+            // 循环播放背景音乐
+            bgMusic.loopFull();
+            // 缓存其他音乐
+            var scoreMusic = game.add.audio('scoreMusic');
+            var bomMusic = game.add.audio('bomMusic');
+            
+              // 添加背景
+              var bg = game.add.image(0, 0, 'bg');
+              bg.width = game.world.width;
+              bg.height = game.world.height;
+  
+              var title = game.add.text(game.world.centerX, game.world.height * 0.25, '接苹果',{
+                  fontSize: '40px',
+                  fontWeight: 'bold',
+                  fill: '#f2bb15'
+              });
+              title.anchor.setTo(0.5, 0.5);
+  
+              var remind = game.add.text(game.world.centerX, game.world.centerY, '点击任意位置开始',{
+                  fontSize: '20px',
+                  fill: '#f2bb15'
+              });
+              remind.anchor.setTo(0.5, 0.5);
+  
+              // 主角
+              var man = game.add.sprite(game.world.centerX, game.world.height * 0.75, 'dude');
+              var manImage = game.cache.getImage('dude');
+              man.width = game.world.width * 0.2;
+              man.height = man.width/manImage.width * manImage.height;
+              man.anchor.setTo(0.5, 0.5);
+
+              // 添加分数
+              var scoreText = game.add.text(game.world.centerX, game.world.height * 0.2, '0', {
+                fontSize: '40px',
+                fontWeight: 'bold',
+                fill: '#f2bb15'
+              });
+              scoreText.anchor.setTo(0.5, 0.5);
+
+              // 移动，滑动监听
+              var touching = false;// 消除pc端和移动端触发差异
+              game.input.onDown.add(function (pointer) {
+                  if(Math.abs(pointer.x - man.x) < man.width/2){
+                      touching = true;
+                  }
+              })
+              game.input.onUp.add(function () {
+                  touching =  false;
+              })
+              game.input.addMoveCallback(function(pointer, x, y, isTap) {
+                  if(!isTap && touching) {
+                      man.x = x;
+                  }
+              })
+  
         }
         
     },
     // 游戏结束
     over: function () {
-        console.log('over');
         this.create = function () {
             this.stage.backgroundColor = '#372';
             setTimeout(function() {
--- phaser-api.md
+++ phaser-api.md
@@ -56,8 +56,127 @@ play: function() {
 }
 ```
 
-### 启动游戏
+## 启动游戏
 
 ```
 game.state.start('preload');
 ```
+
+## Phaser.Loader
+
+用来加载游戏资源
+
+### 加载资源
+
+- 加载图片
+
+```
+game.load.image('star', 'star.png');
+```
+
+- 加载音频
+
+```
+game.load.audio('bg', 'bg.mp3)');
+```
+
+- 加载图片序列
+
+由于要指定帧的宽高，因此一般是动画的连续帧，例如行走动画的每一帧合成的图片。
+
+```
+game.load.spritesheet('walk', 'walk.png', 80, 80);
+```
+
+- 加载资源集合
+
+包括一个json和一张合成的图片，json描述了合成图片中每张图片的宽高位置等信息
+
+```
+game.load.altasJSONArray('fly', 'fly.png', 'fly.json');
+```
+
+
+
+### 加载完成监听
+
+- 监听加载完一个文件的事件
+
+```
+game.load.onFileComplete.add(function(process) {
+   
+})
+```
+
+- 监听加载完成
+
+```
+game.load.onLoadComplete.add(function() {
+     
+})
+```
+
+## game.add
+
+用来快速创建常用的游戏对象
+
+- 添加背景
+
+```
+game.add.image(0, 0, 'bg');// bg是game.load.xxx()中对应的key名
+```
+
+- 添加文字
+
+```
+game.add.text(game.world.centerX, game.world.centerY, '开始游戏',{
+    fontSize: '20px',
+    fill: '#f2bb15'
+});
+```
+
+- 加载游戏精灵
+
+```
+sprite(x, y, key, frame, group)
+```
+
+## Phaser.Input
+
+输入事件管理器，包括鼠标，键盘，触摸，MSPointer等触发事件。
+
+- onTap：点击事件
+
+- onDown：对应touchstart/mousedown
+
+- onUp：对应touchend/mouseup
+
+- onHold：长按事件
+
+- addMoveCallback(callback): 监听滑动事件, callback回调参数：pointer移动点对象，x pointer的x坐标，y pointer的y坐标，isTap是否是“点击”事件的结果。pc端鼠标移上去即触发，移动端需要触摸后触发
+
+- deleteMoveCallback: 移除滑动监听事件
+
+
+
+## 其他对象
+
+### game.world
+
+游戏页面对象，可以调用页面宽高，中心点
+
+```
+game.world.centerX
+game.world.centerY
+game.world.width
+game.world.height
+```
+
+### game.cache
+获取对象，可以通过唯一的key获取到缓存中的图像、声音、视频、JSON等。获取到不是副本，修改对象其他使用该对象的地方也会跟着改变
+通过phaser.loader可以缓存各种资产对象。
+
+```
+game.cache.getImage('people');
+```
+
